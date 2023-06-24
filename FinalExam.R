c07ee1204c9564b33f804635350fbf92bdb68bbf
ilaydaonder <42554143+ilaydaonder@users.noreply.github.com>
2023-06-24 11:32:34 -0400

    Add files via upload

---
+++ FinalExam.R
@@ -0,0 +1,21 @@
+#### You may use this RScript to take your exam. ####
+#### The code below will import the data for you. ####
+#### The datasets are also on CANVAS. You may download and import them on your own if you prefer. ####
+
+library(stargazer)
+
+## Question 3 ##
+logitdataset<-read.csv("https://raw.githubusercontent.com/Brandon-Bolte/PLSC309_F22/main/FL2003.csv")
+
+
+
+
+## Question 4 ##
+electionsdataset<-read.csv("https://raw.githubusercontent.com/Brandon-Bolte/PLSC309_F22/main/elections.csv")
+
+
+
+
+
+
+
